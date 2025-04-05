03cbaa05d3f49c82eb85eafd676091ed107c633a
omarameen2005 <omarmeen7@gmail.com>
2025-04-05 19:57:54 +0200

    Update App.java

--- src/main/java/com/fivestarhotel/App.java
+++ src/main/java/com/fivestarhotel/App.java
@@ -2,6 +2,6 @@ package com.fivestarhotel;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("A7a");
+        System.out.println("Omar has been here");
     }
 }
