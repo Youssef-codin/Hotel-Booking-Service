5670cf839936d000ac32aec7792243a891a18e08
Joe-arch <youssef240100538@sut.edu.eg>
2025-05-19 16:21:39 +0300

    done

--- src/main/java/com/fivestarhotel/App.java
+++ src/main/java/com/fivestarhotel/App.java
@@ -4,32 +4,18 @@ import com.fivestarhotel.Database.Db;
 import com.fivestarhotel.GUI.BookItLogin;
 import com.fivestarhotel.users.Admin;
 
-
 public class App {
     public static void main(String[] args) throws Exception {
         /*
          * WELCOME TO BOOKIT!
-         * START BY CONNECTING TO YOUR OWN DATABASE SERVER, Use Db.connect() and add
+         * START BY CONNECTING TO YOUR OWN DATABASE SERVER (make sure you run the
+         * SQLScript), Use Db.connect() and add
          * your username and password
-         *
-         * Afterwards use Db.select.loadRates(); to load in the rates from the database
-         *
-         * Use Db.select.loadRates(); to load the latest rates from the DB to the
-         * program
          *
          * Login with admin@gbookit.com and Pass_123
          */
 
-        // TODO: After successful login, transition to:
-        // Simplified authentication to use hardcoded credentials:
-        // Admin: admin@bookit.com / Admin_123
         Db.connect("root", "6831");
-
-        // Db.create.signUpUser(new Admin("admin", "admin", "admin@bookit.com",
-        // "Admin_123"));
-
-
-        // Db.connect("root", "");
         Db.create.signUpUser(new Admin("admin", "admin", "admin@bookit.com", "Pass_123"));
         Db.select.loadRates();
 
