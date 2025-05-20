14027f551e685b79177107d27928eee459a0feae
omarameen2005 <omarmeen7@gmail.com>
2025-05-20 19:17:25 +0300

    fixed  ⁠verify customer does not check for letters and wont warn users abt not using letters

--- src/main/java/com/fivestarhotel/GUI/RoomManagement.java
+++ src/main/java/com/fivestarhotel/GUI/RoomManagement.java
@@ -1347,19 +1347,27 @@ public class RoomManagement extends JFrame {
     }
 
     private int verifyCustomerAction() {
+        String customerIdText = customerIdField.getText();
         if (customerIdField.getText().isEmpty()) {
             customerInfoLabel.setText("Please enter a customer ID");
             customerInfoLabel.setForeground(Color.BLACK);
             return -1;
         }
 
-        int customerId = Integer.parseInt(customerIdField.getText());
+        int customerId;
+    try {
+        customerId = Integer.parseInt(customerIdText);
+        } catch (NumberFormatException e) {
+        customerInfoLabel.setText("<html><b>Invalid input</b> - Please enter a valid numerical customer ID</html>");
+        customerInfoLabel.setForeground(Color.RED);
+        return -1;
+        }
 
-        if (customerId <= 0) {
+        if (customerId <= 0 && customerIdField.getText().isEmpty()) {
             customerInfoLabel.setText("<html><b>Invalid input</b> - please enter a valid customer ID</html>");
             customerInfoLabel.setForeground(Color.RED);
             return -1;
-        } else {
+        }else if (customerId > 0 ) {
 
             User user = Db.select.getUserById(UserRoles.CUSTOMER, customerId);
             if (user != null) {
@@ -1369,6 +1377,10 @@ public class RoomManagement extends JFrame {
                 return customerId;
             }
             customerInfoLabel.setText("<html><b>Customer not found</b> - please register new customer</html>");
+            customerInfoLabel.setForeground(Color.RED);
+            return -1;
+        }else {
+            customerInfoLabel.setText("<html><b>Invalid input</b> - please enter a valid customer ID</html>");
             customerInfoLabel.setForeground(Color.RED);
             return -1;
         }
